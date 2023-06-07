8392ca10c177d680b071ca1012676c5d25c34be2
HuiZY <62207399+HZY-97@users.noreply.github.com>
2023-06-07 16:15:05 +0800

    订阅lidar的odom记录gps的odom，足够20个做icp，添加SIASUNLog，链接点云库

--- include/gps_trans.hpp
+++ include/gps_trans.hpp
@@ -10,7 +10,16 @@
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
 #include <vector>
+#include <deque>
 #include <ros/ros.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/registration/icp.h>
+#include <mutex>
+#include <thread>
+#include <chrono>
+
+#include "siasunLog.h"
 
 class GpsTransform
 {
@@ -22,10 +31,28 @@ private:
 
     bool initialized_ = false;
     Eigen::Matrix3d matrix_ecef_2_enu_; // ecef到enu的变换矩阵
-    Eigen::Matrix3d matrix_enu_2_body_; // enu到body的变换矩阵
 
+    // enu2body
+    Eigen::Matrix3d matrix_enu_2_body_; // enu到body的变换矩阵
+    std::mutex matrix_final_theta_mutex;
+    Eigen::Matrix3d matrix_final_theta; // 用于更新最后最后差一点的theta
+
+    // sub odom
+    std::mutex lidar_odom_deq_mutex;
+    std::deque<nav_msgs::Odometry> lidar_odom_deq;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr lidar_odom_pcl;
+
+    // gps odom
+    std::mutex gps_odom_deq_mutex;
+    std::deque<nav_msgs::Odometry> gps_odom_deq;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr gps_odom_pcl;
+
+    // publish
     nav_msgs::Path path_enu_;                    // 换到ENU坐标系下的轨迹
     nav_msgs::Odometry odom_rtk_;
+
+    // for update final theta thread
+    std::thread calFinalThetaThread;
 
     std::vector<Eigen::Vector3d> path_ecef_;
 
@@ -33,6 +60,8 @@ private:
     bool isFirstGps;
     int usefulNums;
     float degVec[4];
+
+    bool isOverFinalTheta; // 是否初始化完 matrix_final_theta
 
     // 世界坐标系信息
     struct
@@ -49,6 +78,8 @@ private:
 
     void Enu2BodyMatrix(const double &current_deg_with_north);
     Eigen::Vector3d Enu2Body(const double &before_x, const double &before_y, const double &before_z);
+    void UpdateFinalThetaMatrix();
+    void StartIcp(pcl::PointCloud<pcl::PointXYZ>::Ptr & lidar_odom_pcl_, pcl::PointCloud<pcl::PointXYZ>::Ptr & gps_odom_pcl_);
 
 public:
     GpsTransform();
@@ -56,6 +87,8 @@ public:
     void add_gps_msg(const sensor_msgs::NavSatFix::ConstPtr &msg);
     nav_msgs::Path path_enu() { return path_enu_; };
     nav_msgs::Odometry odom_enu() { return odom_rtk_; };
+    void addLidarOdom(const nav_msgs::Odometry lidar_msg);
+    void addGpsOdom(const nav_msgs::Odometry gps_msg);
 };
 
 GpsTransform::GpsTransform()
@@ -65,6 +98,18 @@ GpsTransform::GpsTransform()
 
     isFirstGps = true;
     usefulNums = 0;
+    matrix_final_theta.Identity();
+
+    lidar_odom_pcl.reset(new pcl::PointCloud<pcl::PointXYZ>);
+    gps_odom_pcl.reset(new pcl::PointCloud<pcl::PointXYZ>);
+
+    isOverFinalTheta = false;
+    calFinalThetaThread = std::thread(
+        [&]()->void
+        {
+            UpdateFinalThetaMatrix();
+        });
+    calFinalThetaThread.detach();
 }
 
 //这个函数还没有测试过
@@ -118,10 +163,10 @@ void GpsTransform::add_gps_msg(const sensor_msgs::NavSatFix::ConstPtr &msg)
         pose.pose.position.y = point_enu.y();
         pose.pose.position.z = point_enu.z();
 
-        // Eigen::Vector3d afterPose = Enu2Body(point_enu.x(),point_enu.y(),point_enu.z());
-        // pose.pose.position.x = afterPose.x();
-        // pose.pose.position.y = afterPose.y();
-        // pose.pose.position.z = afterPose.z();
+        Eigen::Vector3d afterPose = Enu2Body(point_enu.x(),point_enu.y(),point_enu.z());
+        pose.pose.position.x = afterPose.x();
+        pose.pose.position.y = afterPose.y();
+        pose.pose.position.z = afterPose.z();
 
         path_enu_.poses.push_back(pose);
         
@@ -129,6 +174,7 @@ void GpsTransform::add_gps_msg(const sensor_msgs::NavSatFix::ConstPtr &msg)
         odom_rtk_.header.stamp=msg->header.stamp;
         odom_rtk_.pose.pose.position=pose.pose.position;
 
+        addGpsOdom(odom_rtk_);
     }
     else
     {
@@ -214,10 +260,10 @@ Eigen::Vector3d GpsTransform::Enu2Body(const double &before_x, const double &bef
     beforePose << before_x, before_y, before_z;
     afterPose = matrix_enu_2_body_ * beforePose;
     // afterPose = beforePose;
+    std::unique_lock<std::mutex> theta_mutex(matrix_final_theta_mutex);
+    afterPose = matrix_final_theta * afterPose;
 
-    // std::cout<<"==========================="<<std::endl;
-    // std::cout<<"x:"<<before_x<<"; y:"<<before_y<<"; z:"<<before_z<<std::endl;
-    // std::cout<<"x:"<<afterPose.x()<<"; y:"<<afterPose.y()<<"; z:"<<afterPose.z()<<std::endl;
+    // SIA_INFO("x:%f y:%f z:%f", afterPose.x(), afterPose.y(), afterPose.z());
 
     return afterPose;
 }
@@ -225,4 +271,94 @@ Eigen::Vector3d GpsTransform::Enu2Body(const double &before_x, const double &bef
 Eigen::Vector3d GpsTransform::Ecef2Enu(const Eigen::Vector3d &xyz)
 {
     return matrix_ecef_2_enu_*(xyz- world_orign_.xyz_ecef);
+}
+
+void GpsTransform::addLidarOdom(const nav_msgs::Odometry lidar_msg){
+    std::unique_lock<std::mutex> lidar_lock(lidar_odom_deq_mutex);
+    lidar_odom_deq.emplace_back(lidar_msg);
+    SIA_DEBUG("Add Lidar odom size: %d",lidar_odom_deq.size());
+}
+
+void GpsTransform::addGpsOdom(const nav_msgs::Odometry gps_msg){
+    std::unique_lock<std::mutex> gps_lock(gps_odom_deq_mutex);
+    gps_odom_deq.emplace_back(gps_msg);
+    SIA_DEBUG("Add gps odom size: %d",gps_odom_deq.size());
+}
+
+void GpsTransform::UpdateFinalThetaMatrix(){
+    while (true)
+    {
+        // SIA_DEBUG("Now In thread UpdateFinalThetaMatrix");
+        if(!isOverFinalTheta)
+        {
+            SIA_DEBUG("Thread::UpdateFinalThetaMatrix::if(!isOverFinalTheta)");
+            if(20 < lidar_odom_deq.size())
+            {
+                // TODO update lidar_odom_deq_mutex
+                std::unique_lock<std::mutex> lidar_lock(lidar_odom_deq_mutex);
+                std::unique_lock<std::mutex> gps_lock(gps_odom_deq_mutex);
+
+                for(int i = 0; i < lidar_odom_deq.size(); i++)
+                {
+                    pcl::PointXYZ tmpPoint;
+                    tmpPoint.x = lidar_odom_deq[i].pose.pose.position.x;
+                    tmpPoint.y = lidar_odom_deq[i].pose.pose.position.y;
+                    tmpPoint.z = lidar_odom_deq[i].pose.pose.position.z;
+                    lidar_odom_pcl->push_back(tmpPoint);
+                }
+                
+                for(int i = 0; i < gps_odom_deq.size(); i++)
+                {
+                    pcl::PointXYZ tmpPoint;
+                    tmpPoint.x = gps_odom_deq[i].pose.pose.position.x;
+                    tmpPoint.y = gps_odom_deq[i].pose.pose.position.y;
+                    tmpPoint.z = gps_odom_deq[i].pose.pose.position.z;
+                    gps_odom_pcl->push_back(tmpPoint);
+                }
+
+                StartIcp(lidar_odom_pcl, gps_odom_pcl);
+            }
+        }else
+        {
+            SIA_DEBUG("Thread::UpdateFinalThetaMatrix::else(!isOverFinalTheta)");
+            std::unique_lock<std::mutex> lidar_lock(lidar_odom_deq_mutex);
+            std::unique_lock<std::mutex> gps_lock(gps_odom_deq_mutex);
+            lidar_odom_deq.clear();
+            gps_odom_deq.clear();
+        }
+
+        // 等待 100 毫秒
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void GpsTransform::StartIcp(pcl::PointCloud<pcl::PointXYZ>::Ptr & lidar_odom_pcl_, pcl::PointCloud<pcl::PointXYZ>::Ptr & gps_odom_pcl_)
+{
+    SIA_INFO("Now ICP Start");
+    pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+    icp.setMaxCorrespondenceDistance(0.05);
+    icp.setMaximumIterations(50);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr aligned(new pcl::PointCloud<pcl::PointXYZ>);
+    icp.setInputSource(lidar_odom_pcl_);
+    icp.setInputTarget(gps_odom_pcl_);
+    icp.align(*aligned);
+
+    if (icp.hasConverged())
+    {
+        SIA_INFO("ICP converged, score: %f",icp.getFitnessScore());
+        std::unique_lock<std::mutex> theta_mutex(matrix_final_theta_mutex);
+        Eigen::Affine3f tmpT;
+        tmpT = icp.getFinalTransformation();
+        matrix_final_theta = tmpT.linear().cast<double>();
+        std::cout << "---------------matrix_final_theta :" << std::endl << matrix_final_theta << std::endl;
+        // matrix_final_theta = tmpT.rotation().cast<Eigen::Matrix3d>();
+        isOverFinalTheta = true;
+    }
+    else
+    {
+        SIA_ERROR("ICP did not converge");
+    }
+
+    aligned.reset();
 }
--- src/gps_trans.cpp
+++ src/gps_trans.cpp
@@ -2,7 +2,6 @@
 // time: 2023.04.14
 // faction: 本文件的作用是将GPS轨迹转换到世界坐标系下去
 
-
 #include "../include/gps_trans.hpp"
 
 ros::Publisher path_pub;
@@ -11,28 +10,37 @@ GpsTransform gps_transform;
 
 void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr &msg)
 {
-    // 处理GPS数据                         排名
-    // 0-未定位或无效解                      5
-    // 1-单点定位                          4
-    // 4-定位 RTK 固定解                    1
-    // 5-定位 RTK 浮点解                    2
-    // 6-INS 定位解或 GNSS/INS 组合定位解     3
-
-    if(4 == msg->status.status){
+    // if (sensor_msgs::NavSatStatus::STATUS_FIX == msg->status.status)
+    // {
+        // SIA_DEBUG("*******************************");
+        // SIA_DEBUG("Current GPS State:%d", msg->status.status);
+        
         gps_transform.add_gps_msg(msg);
         path_pub.publish(gps_transform.path_enu());
         odom_pub.publish(gps_transform.odom_enu());
-    }
+    // }
 }
 
-int main(int argc, char** argv)
+void odomCallback(const nav_msgs::Odometry::ConstPtr &msg)
 {
-    ros::init(argc, argv, "my_node");
+    nav_msgs::Odometry tmpMsg;
+    tmpMsg = *msg;
+    gps_transform.addLidarOdom(tmpMsg);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "gps_trans_node");
     ros::NodeHandle nh;
 
+    // subscriber
     ros::Subscriber gps_sub = nh.subscribe<sensor_msgs::NavSatFix>("/hc_driver/gps_data", 10, gpsCallback);
+    ros::Subscriber lidar_odom_sub = nh.subscribe<nav_msgs::Odometry>("/lio_sam/mapping/odometry", 1, odomCallback);
+
+    // publish
     path_pub = nh.advertise<nav_msgs::Path>("/gps_path_enu", 10);
     odom_pub = nh.advertise<nav_msgs::Odometry>("/gps_odom_enu", 10);
+
     ros::spin();
 
     return 0;
